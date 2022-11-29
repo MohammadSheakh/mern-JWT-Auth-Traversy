bb4559512eb01480635bae667bd83dc86b95f16f
MohammadSheakh <mohammad.sheakh@gmail.com>
2022-11-29 21:49:59 +0600

    update with comments

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,89 +1,123 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
-const asyncHandler = require('express-async-handler')
-const User = require('../models/userModel')
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
 // @desc    Register new user
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body
+    // asyncHandler use kora hoy .. exception handle korar jonno
+    /**
+     * jokhon amra ei endpoint e request send korbo .. tokhon request er body te kichu information ashbe
+     *
+     */
+    const { name, email, password } = req.body;
 
-  if (!name || !email || !password) {
-    res.status(400)
-    throw new Error('Please add all fields')
-  }
+    // little bit validation
+    if (!name || !email || !password) {
+        res.status(400); // bad request
+        throw new Error("Please add all fields");
+    }
 
-  // Check if user exists
-  const userExists = await User.findOne({ email })
+    // Check if user exists
+    const userExists = await User.findOne({ email }); // jei email ashse .. shei email diye jodi kono user
+    // already exist kore ..
 
-  if (userExists) {
-    res.status(400)
-    throw new Error('User already exists')
-  }
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exists"); // ekta error pathabo
+    }
 
-  // Hash password
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(password, salt)
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-  // Create user
-  const user = await User.create({
-    name,
-    email,
-    password: hashedPassword,
-  })
+    // Create user // user jehetu exist kore na .. finally amra user create korbo ..
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword, // hashed password ta pass korbo
+    });
 
-  if (user) {
-    res.status(201).json({
-      _id: user.id,
-      name: user.name,
-      email: user.email,
-      token: generateToken(user._id),
-    })
-  } else {
-    res.status(400)
-    throw new Error('Invalid user data')
-  }
-})
+    if (user) {
+        res.status(201).json({
+            // something is created .. // password kintu send korbo na ..
+            _id: user.id, // create hoile .. ekta id paowa jabe ...
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
+});
 
 // @desc    Authenticate a user
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body
+    const { email, password } = req.body;
+    /**
+     * jokhon amra ei endpoint e request send korbo .. tokhon request er body te kichu information ashbe
+     */
 
-  // Check for user email
-  const user = await User.findOne({ email })
+    // Check for user email
+    const user = await User.findOne({ email }); // email data base e khuje paowa jay kina .. check korbo
 
-  if (user && (await bcrypt.compare(password, user.password))) {
-    res.json({
-      _id: user.id,
-      name: user.name,
-      email: user.email,
-      token: generateToken(user._id),
-    })
-  } else {
-    res.status(400)
-    throw new Error('Invalid credentials')
-  }
-})
+    if (user && (await bcrypt.compare(password, user.password))) {
+        // user jodi khuje paowa jay and req.body er password er shathe jodi user er password er match kore
+        // taile amra response e user er shob information pathabo .. tar shathe generateToken nam er
+        // function er maddhome user er id pathiye .. token generate kore .. token send kore dibo ..
+
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(400); // bad requst ..
+        throw new Error("Invalid credentials");
+    }
+});
 
 // @desc    Get user data
 // @route   GET /api/users/me
 // @access  Private
+/**
+ * login korar pore get me korbo .. so .. login korar pore front-end e token ta pathiye disi ..
+ * sheta may be shekhan e request er headers e add hoye jabe .. bearer "token" ei format e ..
+ *
+ * tar por jokhon getMe call hobe .. mane she ekhon user er information jante chay .. tokhon amra
+ * amader JWT secret diye token ta verify kore dekhbo .. thik thak thakle .. req.user er moddhe
+ * user er information ta vore diye .. next() call kore dibo .. ei hocche kahini ..
+ *  */
 const getMe = asyncHandler(async (req, res) => {
-  res.status(200).json(req.user)
-})
+    // protect middleware er moddhe req.user er moddhe user er informatio diye deowa hoyeche .. tai
+    // ekhane access korte partesi ..
+    res.status(200).json(req.user); // logged in user er shob information send korbo ..
+    /// 😀
+    // const { _id, name, email } = await User.findById(req.user.id);
 
-// Generate JWT
+    // res.status(200).json({
+    //     id: _id,
+    //     name,
+    //     email,
+    // });
+});
+
+// Generate JWT // token generate korte .. user er id ta lage .. ar JWT Secret ta lage .. option e
+// expire time ta bole dite hoy ...
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: '30d',
-  })
-}
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+    });
+};
 
 module.exports = {
-  registerUser,
-  loginUser,
-  getMe,
-}
+    registerUser,
+    loginUser,
+    getMe,
+};
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,36 +1,46 @@
-const jwt = require('jsonwebtoken')
-const asyncHandler = require('express-async-handler')
-const User = require('../models/userModel')
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+/**
+ * now we want to be able to protect routes.. eta amra custom middleware er maddhome kore thaki ..
+ * routes file e use korbo ei middleware ..
+ */
 
 const protect = asyncHandler(async (req, res, next) => {
-  let token
+    let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
-    try {
-      // Get token from header
-      token = req.headers.authorization.split(' ')[1]
+    if (
+        req.headers.authorization && // request er headers e token ta ashbe ..
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        try {
+            // Get token from bearer header
+            token = req.headers.authorization.split(" ")[1]; // " " diye split korle .. array te shift hobe
+            // shetar 1 number index e token ta thake.. 0 number index e bearer word ta thake
 
-      // Verify token
-      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            // Verify token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      // Get user from the token
-      req.user = await User.findById(decoded.id).select('-password')
+            // Get user from the token // token has userId as payload .. when we generate token .. we give
+            // id of user who want to log in or register ..
+            req.user = await User.findById(decoded.id).select("-password"); // joto gula route protected
+            // decoded.id .. eta karon generate token er moddhe amra id pass korechilam .. shekhan e amra
+            // onek kichui send korte pari .. shegula amra ekhane pabo .. name o send korte pari ..
+            // shob jaygay req.user er moddhe user er information ta pabo .. amra hashed password chai na ..
+            // tai password er age ekta minus diye nilam ..  password include hobe na ..
 
-      next()
-    } catch (error) {
-      console.log(error)
-      res.status(401)
-      throw new Error('Not authorized')
+            next(); // next middleware call korbo
+        } catch (error) {
+            console.log(error);
+            res.status(401); // 401 mane holo not authorized ...
+            throw new Error("Not authorized"); // Error throw kortesi ..
+        }
     }
-  }
 
-  if (!token) {
-    res.status(401)
-    throw new Error('Not authorized, no token')
-  }
-})
+    if (!token) {
+        res.status(401);
+        throw new Error("Not authorized, no token");
+    }
+});
 
-module.exports = { protect }
+module.exports = { protect };
